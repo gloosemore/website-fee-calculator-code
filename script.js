a254c80b8e68b16bd8c658401f432054a39ceafd
gloosemore <g.m.loosemore@gmail.com>
2025-02-23 16:05:42 -0500

    ok

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+const pricePkgSTANDARD = 150;
+const pricePkgEMPLOYEE_ONLY = 115;
+const pricePkgDISABLED = 125;
+const pricePkgSENIORS = 135;
+const pricePkgGIG_ECONOMY = 175;
+const pricePkgHOME_DAY_CARE = 185;
+const pricePkgFULL_TIME_STUDENTS = 85;
+const priceAddOnBUSINESS = 50;
+const priceAddOnRENT = 50;
+
 var mePkgType = "";
-var meBusAddOns = 0;
-var meRentAddOns = 0;
 var partPkgType = "";
+
+var meBusAddOns = 0;
+var meBusAddOnsDiscount = 0;
 var partBusAddOns = 0;
+var partBusAddOnsDiscount = 0;
+
+var meRentAddOns = 0;
+var meRentAddOnsDiscount = 0;
 var partRentAddOns = 0;
+var partRentAddOnsDiscount = 0;
 
 $(document).ready(function () {
   fillPkgGrid();
@@ -12,28 +28,55 @@ $(document).ready(function () {
   $(".pkg-me-but").click(function () {
     const buttonId = $(this).attr("id");
     console.log("buttonId=", buttonId);
-    const pkgType = buttonId.substring(11, 30);
-    console.log("pkgType=", pkgType);
+    mePkgType = buttonId.substring(11, 30).replace(" ", "_").replace("-", "_");
+    console.log("mePkgType=", mePkgType);
 
-    mePkgType = pkgType;
+    meBusAddOnsDiscount = 0;
+    meRentAddOnsDiscount = 0;
+    switch (mePkgType) {
+      case "GIG_ECONOMY":
+        meBusAddOnsDiscount = 2;
+        break;
+      case "HOME_DAY_CARE":
+        meBusAddOnsDiscount = 1;
+        break;
+    }
+
     $(".pkg-me-but")
       .removeClass("btn-style-success")
       .addClass("btn-style-primary");
     $(this).removeClass("btn-style-primary").addClass("btn-style-success");
+
+    updatePrices();
   });
 
   // Handle "Partner" button clicks
   $(".pkg-part-but").click(function () {
     const buttonId = $(this).attr("id");
     console.log("buttonId=", buttonId);
-    const pkgType = buttonId.substring(11, 30);
-    console.log("pkgType=", pkgType);
+    partPkgType = buttonId
+      .substring(11, 30)
+      .replace(" ", "_")
+      .replace("-", "_");
+    console.log("partPkgType=", partPkgType);
 
-    partPkgType = pkgType;
+    partBusAddOnsDiscount = 0;
+    partRentAddOnsDiscount = 0;
+    switch (partPkgType) {
+      case "GIG_ECONOMY":
+        partBusAddOnsDiscount = 2;
+        break;
+      case "HOME_DAY_CARE":
+        partBusAddOnsDiscount = 1;
+        break;
+    }
+
     $(".pkg-part-but")
       .removeClass("btn-style-success")
       .addClass("btn-style-primary");
     $(this).removeClass("btn-style-primary").addClass("btn-style-success");
+
+    updatePrices();
   });
 });
 
@@ -48,37 +91,37 @@ const fillPkgGrid = function () {
     {
       name: "STANDARD",
       desc: "<p><b><i>Core Tax Service</i></b> for most individuals</p>",
-      price: "$150",
+      price: "$" + pricePkgSTANDARD,
     },
     {
       name: "EMPLOYEE ONLY",
       desc: "<p><b><i>Core Tax Service</i></b> for workers with only employment income</p>",
-      price: "$115",
+      price: "$" + pricePkgEMPLOYEE_ONLY,
     },
     {
-      name: "DISABILITY",
+      name: "DISABLED",
       desc: "<p><b><i>Core Tax Service</i></b> for those with disability benefits</p>",
-      price: "$120",
+      price: "$" + pricePkgDISABLED,
     },
     {
       name: "SENIORS",
       desc: "<p><b><i>Core Tax Service</i></b> for those 65 or over</p>",
-      price: "$135",
+      price: "$" + pricePkgSENIORS,
     },
     {
       name: "GIG ECONOMY",
       desc: "<p><b><i>Core Tax Service</i></b> for gig workers. Also includes <b><i>2 Business</i></b> Add-Ons</p>",
-      price: "$175",
+      price: "$" + pricePkgGIG_ECONOMY,
     },
     {
       name: "HOME DAY CARE",
       desc: "<p><b><i>Core Tax Service</i></b> for day care providers. Also includes <b><i>1 Business</i></b> Add-On</p>",
-      price: "$185",
+      price: "$" + pricePkgHOME_DAY_CARE,
     },
     {
       name: "FULL-TIME STUDENTS",
       desc: "<p><b><i>Core Tax Service</i></b> for students</p>",
-      price: "$85",
+      price: "$" + pricePkgFULL_TIME_STUDENTS,
     },
   ];
   var currPkgNumb = 1;
@@ -123,6 +166,95 @@ const fillPkgGrid = function () {
 
   html += "</div>";
   $("#pkg-container").html(html);
+};
+
+const updatePrices = function () {
+  var priceMeCore = 0;
+  switch (mePkgType) {
+    case "STANDARD":
+      priceMeCore = pricePkgSTANDARD;
+      break;
+    case "EMPLOYEE_ONLY":
+      priceMeCore = pricePkgEMPLOYEE_ONLY;
+      break;
+    case "DISABLED":
+      priceMeCore = pricePkgDISABLED;
+      break;
+    case "SENIORS":
+      priceMeCore = pricePkgSENIORS;
+      break;
+    case "GIG_ECONOMY":
+      priceMeCore = pricePkgGIG_ECONOMY;
+      break;
+    case "HOME_DAY_CARE":
+      priceMeCore = pricePkgHOME_DAY_CARE;
+      break;
+    case "FULL_TIME_STUDENTS":
+      priceMeCore = pricePkgFULL_TIME_STUDENTS;
+      break;
+  }
+
+  var pricePartCore = 0;
+  switch (partPkgType) {
+    case "STANDARD":
+      pricePartCore = pricePkgSTANDARD;
+      break;
+    case "EMPLOYEE_ONLY":
+      pricePartCore = pricePkgEMPLOYEE_ONLY;
+      break;
+    case "DISABLED":
+      pricePartCore = pricePkgDISABLED;
+      break;
+    case "SENIORS":
+      pricePartCore = pricePkgSENIORS;
+      break;
+    case "GIG_ECONOMY":
+      pricePartCore = pricePkgGIG_ECONOMY;
+      break;
+    case "HOME_DAY_CARE":
+      pricePartCore = pricePkgHOME_DAY_CARE;
+      break;
+    case "FULL_TIME_STUDENTS":
+      pricePartCore = pricePkgFULL_TIME_STUDENTS;
+      break;
+  }
+
+  const priceTotalCore = priceMeCore + pricePartCore;
+
+  $("#price-me-core").html(priceMeCore);
+  $("#price-part-core").html(pricePartCore);
+  $("#price-total-cire").html(priceTotalCore);
+
+  // 	// business add-ons
+  //   meBusAddOnsDiscount = 0;
+  //   switch (mePkgType) {
+  //     case "GIG ECONOMY":
+  //       meBusAddOnsDiscount = 2;
+  //       break;
+  //     case "HOME DAY CARE":
+  //       meBusAddOnsDiscount = 1;
+  //       break;
+  //   }
+  //   const meBillableBusAddOns = meBusAddOns - meBusAddOnsDiscount;
+  //   partBusAddOnsDiscount = 0;
+  //   switch (partPkgType) {
+  //     case "GIG ECONOMY":
+  //       partBusAddOnsDiscount = 2;
+  //       break;
+  //     case "HOME DAY CARE":
+  //       partBusAddOnsDiscount = 1;
+  //       break;
+  //   }
+  //   const partBillableBusAddOns = partBusAddOns - partBusAddOnsDiscount;
+  //   priceBusTotal = (meBillableBusAddOns + partBillableBusAddOns) * busAddOnPrice;
+  //   // rental add ons ( no discounts)
+  //   meRentAddOnsDiscount = 0;
+  //   const meBillableRentAddOns = meRentAddOns - meRentAddOnsDiscount;
+  //   partRentAddOnsDiscount = 0;
+  //   const partBillableRentAddOns = partRentAddOns - partRentAddOnsDiscount;
+  //   const totalBillableRentAddOns = meBillableRentAddOns + partBillableRentAddOns;
+  //   price;
+  //   priceBusTotal = (meBillableBusAddOns + partBillableBusAddOns) * busAddOnPrice;
 };
 
 // const fillAddOnGrid = function () {
